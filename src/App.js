282f564b09037045f45e9ab2dc4d715331131453
Jeral <jeral.cris@gmail.com>
2021-12-26 17:51:03 -0600

    Consultando la API y leyendo los resultados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import Formulario from './components/Formulario';
@@ -6,13 +6,30 @@ import Formulario from './components/Formulario';
 function App() {
    // definir la categoria y noticias
    const [categoria, setCategoria] = useState('');
+   const [noticias, setNoticias] = useState([]);
+
+   useEffect(() => {
+      const consultarApi = async () => {
+         const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${process.env.REACT_APP_API_KEY}`;
+
+         const respuesta = await fetch(url);
+         const noticias = await respuesta.json();
+
+         setNoticias(noticias.articles);
+      };
+
+      consultarApi();
+   }, [categoria]);
 
    return (
       <>
          <Header titulo='Buscador de Noticias' />
 
          <div className='container white'>
-            <Formulario setCategoria={setCategoria} />
+            <Formulario
+               //
+               setCategoria={setCategoria}
+            />
          </div>
       </>
    );
